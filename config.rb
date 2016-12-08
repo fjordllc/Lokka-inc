120bc4b7d833897073d3cb6c5ad4d8b53baecbec
teppei machida <machida@fjord.jp>
2016-12-08 16:30:14 +0900

    Gem update

--- config.rb
+++ config.rb
@@ -180,6 +180,7 @@ set :markdown, fenced_code_blocks: true, smartypants: true, hard_wrap: true,  au
 set :markdown_engine, :redcarpet
 
 activate :blog do |blog|
+  blog.name   = "articles"
   blog.prefix = "articles"
   blog.permalink = ":year:month:day-:title.html"
   # blog.sources = ":year-:month-:day-:title.html"
@@ -197,6 +198,27 @@ activate :blog do |blog|
   blog.per_page = 10
   blog.page_link = "page/:num"
 end
+
+activate :blog do |blog|
+  blog.name   = "works"
+  blog.prefix = "works"
+  blog.permalink = ":year:month:day-:title.html"
+  # blog.sources = ":year-:month-:day-:title.html"
+  # blog.taglink = "tags/:tag.html"
+  blog.layout = "layout"
+  blog.summary_separator = /(READMORE)/
+  blog.summary_length = 250
+  blog.year_link = ":year.html"
+  blog.month_link = ":year/:month.html"
+  blog.day_link = ":year/:month/:day.html"
+  blog.default_extension = ".markdown"
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/:num"
+end
+
 
 Time.zone = "Tokyo"
 
