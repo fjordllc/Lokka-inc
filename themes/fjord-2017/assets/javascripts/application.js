cb0783713532ae06d3596e33cf2e893de26b4255
teppei machida <machida@fjord.jp>
2017-08-10 19:40:27 +0900

    受託開発の案内ページ作成

--- themes/fjord-2017/assets/javascripts/application.js
+++ themes/fjord-2017/assets/javascripts/application.js
@@ -2,3 +2,5 @@ require('./targetBlank.js');
 require('./footerFixed.js');
 require('./jquery.autosize.min.js');
 require('./jsAutosize.js');
+window.Mojik = require('mojik/mojik.js');
+require('./jsMojik.js');
---
+++ themes/fjord-2017/assets/javascripts/jsMojik.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function(){
+  Mojik.compose(".js-mojik, .js-mojik-text *");
+})
