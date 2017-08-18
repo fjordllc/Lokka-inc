79ee6ba7e9ee151d087e433e2a8a50cd435bc9e5
Masaki Komagata <komagata@gmail.com>
2017-08-18 18:24:34 +0900

    docsを削除しないようにした
    CNAMEファイルも消えてしまうので

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 git clone --depth=1 git@github.com:fjordllc/fjordllc.git master
-rm -rf master/docs
 cp -rp docs master/
 cd master
 git checkout master
 git add docs
-git commit -a -m 'Update articles'
+git commit -a -m "Update articles"
 git push origin master --force
