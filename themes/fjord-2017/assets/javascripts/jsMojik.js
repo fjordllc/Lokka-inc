6d7b6c9ffa96f812dec9d2e2d59e614cf18e00cf
teppei machida <machida@fjord.jp>
2017-08-23 18:37:03 +0900

    一覧のURL修正

--- themes/fjord-2017/assets/javascripts/jsMojik.js
+++ themes/fjord-2017/assets/javascripts/jsMojik.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){
   $('p:empty').remove();
-  Mojik.compose(".js-mojik-text p, .js-mojik-text h2 .js-mojik-text h3 .js-mojik-text h4 .js-mojik-text h5 .js-mojik-text li");
+  Mojik.compose(".js-mojik, .js-mojik-text p, .js-mojik-text h2 .js-mojik-text h3 .js-mojik-text h4 .js-mojik-text h5 .js-mojik-text li");
 })
--- themes/fjord-2017/static/assets/application.js
+++ themes/fjord-2017/static/assets/application.js
@@ -1420,7 +1420,7 @@ module.exports = function(module) {
 
 document.addEventListener("DOMContentLoaded", function () {
   $('p:empty').remove();
-  Mojik.compose(".js-mojik-text p, .js-mojik-text h2 .js-mojik-text h3 .js-mojik-text h4 .js-mojik-text h5 .js-mojik-text li");
+  Mojik.compose(".js-mojik, .js-mojik-text p, .js-mojik-text h2 .js-mojik-text h3 .js-mojik-text h4 .js-mojik-text h5 .js-mojik-text li");
 });
 
 /***/ })
