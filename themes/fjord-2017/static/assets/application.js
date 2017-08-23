e2b4fba6891c5fc95a32be14de23250168601793
teppei machida <machida@fjord.jp>
2017-08-23 16:52:41 +0900

    mojikのエラー回避

--- themes/fjord-2017/static/assets/application.js
+++ themes/fjord-2017/static/assets/application.js
@@ -226,7 +226,8 @@ $(function () {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  Mojik.compose(".js-mojik-text *");
+  $('p:empty').remove();
+  Mojik.compose(".js-mojik-text p, .js-mojik-text h2 .js-mojik-text h3 .js-mojik-text h4 .js-mojik-text h5 .js-mojik-text li");
 });
 
 /***/ }),
