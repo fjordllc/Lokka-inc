99cb866d6ea470ae78214ffbdef719176dc2c441
teppei machida <machida@fjord.jp>
2020-04-22 16:05:09 +0900

    mojik削除

--- themes/fjord-2017/assets/javascripts/application.js
+++ themes/fjord-2017/assets/javascripts/application.js
@@ -3,7 +3,4 @@ require('./jquery.autosize.min.js');
 require('./jsAutosize.js');
 window.SmoothScroll = require('smooth-scroll/dist/js/smooth-scroll.js');
 require('./jsSmoothScroll.js');
-//window.Mojik = require('mojik/mojik.js');
-window.Mojik = require('./mojik.js');
-require('./jsMojik.js');
 require('./inquiry.js');
